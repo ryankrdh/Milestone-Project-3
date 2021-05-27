94f0384567443b158311e594d93bb8dead8caabe
Ryan Kim <ryankrdh@gmail.com>
2021-05-26 21:28:59 -0700

    created the main.py file. Ready to start the project

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Game will have only have one player against a dealer.
+
+Card rank are as follows:
+
+2, 3, 4, 5, 6, 7, 8, 9, 10, Jack(10), Queen(10), King(10), Ace(1 or 11)
+
+Player can either STAND or HIT. You are also able to determine the betting amount.
+'''
+
+import random
+import pdb
